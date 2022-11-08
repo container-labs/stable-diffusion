05da912d4f2dd166e0e3122be2ddc62eb4d25f14
Will <will@containerlabs.io>
2022-11-07 23:18:19 -0800

    diffs

--- docker/main.py
+++ docker/main.py
@@ -9,10 +9,15 @@ gpu_usage()
 
 pipe = StableDiffusionPipeline.from_pretrained(
   "dallinmackay/Van-Gogh-diffusion",
-  use_auth_token=os.environ["HUGGINGFACE_TOKEN"]
-)
+  use_auth_token=os.environ["HUGGINGFACE_TOKEN"],
+  cache_dir="/root/.cache/huggingface"
+).to("mps")
+print("done loading model")
 
 result = pipe(
         "a dog",
         num_inference_steps=10)
 image = result.images[0]
+image.save("/image_dir/dog.png")
+
+print("all done")
