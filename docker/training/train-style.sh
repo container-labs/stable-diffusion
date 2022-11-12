7d90313cd54cfdec3682b0ecbb7cf2eba28bd372
Will <will@containerlabs.io>
2022-11-12 10:32:45 -0800

    output to fuse-mounted data dir

--- docker/training/train-style.sh
+++ docker/training/train-style.sh
@@ -3,13 +3,6 @@
 export MODEL_NAME="runwayml/stable-diffusion-v1-5"
 export DATA_DIR="./data"
 
-# import torch
-# import math
-# # this ensures that the current MacOS version is at least 12.3+
-# print(torch.backends.mps.is_available())
-# # this ensures that the current current PyTorch installation was built with MPS activated.
-# print(torch.backends.mps.is_built())
-
 python textual_inversion.py \
   --pretrained_model_name_or_path=runwayml/stable-diffusion-v1-5 \
   --train_data_dir=./data \
@@ -19,4 +12,7 @@ python textual_inversion.py \
   --train_batch_size=1 \
   --max_train_steps=1000 \
   --learning_rate=5.0e-04 --scale_lr \
-  --output_dir="textual_inversion_bored_ape"
+  # https://cloud.google.com/vertex-ai/docs/training/code-requirements#fuse
+  --output_dir="gs://md-ml/textual_inversion_bored_ape"
+
+
