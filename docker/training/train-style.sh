ef431476451be30770de296f2ffa3c631a98c2bf
Will <will@containerlabs.io>
2022-11-12 23:09:15 -0800

    adding steps arg

--- docker/training/train-style.sh
+++ docker/training/train-style.sh
@@ -18,6 +18,10 @@ for i in "$@"; do
       OUTPUT_DIR="${i#*=}"
       shift # past argument=value
       ;;
+    -s=*|--steps=*)
+      MAX_STEPS="${i#*=}"
+      shift # past argument=value
+      ;;
     --default)
       DEFAULT=YES
       shift # past argument with no value
@@ -35,6 +39,7 @@ echo "Model Name  = ${MODEL_NAME}"
 echo "Data Dir    = ${DATA_DIR}"
 echo "Output Dir  = ${OUTPUT_DIR}"
 echo "DEFAULT     = ${DEFAULT}"
+echo "Max Steps   = ${MAX_STEPS}"
 
 # conda init bash
 eval "$(conda shell.bash hook)"
@@ -49,7 +54,7 @@ python textual_inversion.py \
   --placeholder_token="<bored-ape>" --initializer_token="ape" \
   --resolution=512 \
   --train_batch_size=1 \
-  --max_train_steps=1000 \
+  --max_train_steps=${MAX_STEPS} \
   --learning_rate=5.0e-04 --scale_lr \
   --output_dir=${OUTPUT_DIR}
 
