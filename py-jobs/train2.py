1e40ff9bf33a6116ac2e8bcc1d4cfd9b3eee3a63
Will <will@containerlabs.io>
2022-12-31 14:43:20 -0800

    add params to training job

--- py-jobs/train2.py
+++ py-jobs/train2.py
@@ -12,7 +12,7 @@ MACHINE_TYPE_TRAINING = "a2-highgpu-1g"
 ACCELERATOR_TYPE_TRAINING = "NVIDIA_TESLA_A100"
 # Training doesn't use more than 1 GPU with our config
 ACCELERATOR_COUNT = 1
-MODEL_NAME="jpl_512_dopeaf_5"
+MODEL_NAME="jpl_512_dopeaf_1-5"
 
 # learning rates
 # "low" 2e-6
@@ -23,6 +23,7 @@ CMDARGS = [
     # TODO: upgrade this to use the new stable diffusion model
     # "--model=CompVis/stable-diffusion-v1-4",
     "--model=runwayml/stable-diffusion-v1-5",
+    # "--model=stabilityai/stable-diffusion-2-1",
     "--data=/gcs/md-ml/training-data-styles/jpl-512",
     f"--output=/gcs/md-ml/{JOB_NAME}/model",
     # https://huggingface.co/blog/dreambooth#:~:text=In%20our%20experiments%2C%20a%20learning,learning%20rate%20is%20too%20high.
@@ -32,6 +33,8 @@ CMDARGS = [
     "--repeat=100",
     f"--batch={ACCELERATOR_COUNT}",
     "--learning=5.0e-06",
+    "--mixed_precision=bf16",
+    "--resolution=512",
     "--kind=style",
 ]
 
